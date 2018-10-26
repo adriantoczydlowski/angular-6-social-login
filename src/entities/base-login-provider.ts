858165d92a5d99b2b3fa5fb915c99301c959acba
Adrian Toczydlowski <adrian.toczydlowski@inspeerity.com>
2018-10-26 13:16:16 +0200

    Fix linkedin login

--- src/entities/base-login-provider.ts
+++ src/entities/base-login-provider.ts
@@ -1,5 +1,5 @@
-import { LoginProvider } from './login-provider';
-import { SocialUser, LoginProviderClass } from './user';
+import { LoginProvider } from "./login-provider";
+import { SocialUser, LoginProviderClass } from "./user";
 
 export abstract class BaseLoginProvider implements LoginProvider {
   constructor() {}
@@ -9,14 +9,16 @@ export abstract class BaseLoginProvider implements LoginProvider {
   abstract signOut(): Promise<any>;
 
   loadScript(obj: LoginProviderClass, onload: any): void {
-    if (document.getElementById(obj.name)) { return; }
-    let signInJS = document.createElement('script');
+    if (document.getElementById(obj.name)) {
+      return;
+    }
+    let signInJS = document.createElement("script");
     signInJS.async = true;
     signInJS.src = obj.url;
     signInJS.onload = onload;
-    if (obj.name === 'LINKEDIN') {
+    if (obj.name === "linkedin") {
       signInJS.async = false;
-      signInJS.text = ('api_key: ' + obj.id).replace('\'', '');
+      signInJS.text = ("api_key: " + obj.id).replace("'", "");
     }
     document.head.appendChild(signInJS);
   }
